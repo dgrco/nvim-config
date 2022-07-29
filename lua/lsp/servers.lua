7ebe3bb5a095f807e7485f8403b15d9721a67b6f
dgrco <dantegrieco11@gmail.com>
2022-07-29 16:20:06 -0700

    coq fix

--- lua/lsp/servers.lua
+++ lua/lsp/servers.lua
@@ -5,5 +5,5 @@ lsp_servers = {
 }
 
 for i, server in ipairs(lsp_servers) do
-  require("lspconfig")[server].setup{}
+  require("lspconfig")[server].setup(require("coq").lsp_ensure_capabilities())
 end
